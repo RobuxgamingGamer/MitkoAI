f1be53ae658887487017e5f98ca51967fc3fe654
RobuxgamingGamer <8wfq7hpy4z@privaterelay.appleid.com>
2026-01-09 15:49:32 +0200

    Update math.js

--- math.js
+++ math.js
@@ -1,14 +1,10 @@
 export function handleMath(text) {
-  const clean = text.replace(/\s+/g, "");
-
-  if (/^\d+[\+\-\*\/]\d+$/.test(clean)) {
-    try {
-      const result = eval(clean);
-      return `${clean} = ${result}`;
-    } catch {
-      return null;
+  try {
+    if (/^[0-9+\-*/().\s]+$/.test(text)) {
+      return `${text} = ${eval(text)}`;
     }
+  } catch {
+    return null;
   }
-
   return null;
 }
