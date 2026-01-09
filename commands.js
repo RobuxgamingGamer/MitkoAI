e11fc167b7e350da5868cdb562105e5943dc7391
RobuxgamingGamer <8wfq7hpy4z@privaterelay.appleid.com>
2026-01-09 13:15:47 +0200

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,7 @@
+function isOwner(text) {
+  return text.toLowerCase().includes("owner");
+}
+
+function ownerResponse() {
+  return "My owner is Mitko. Follow him on YouTube: Robux_Gaming_Gamer 😃";
+}
