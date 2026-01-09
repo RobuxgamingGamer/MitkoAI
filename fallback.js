b31e97dde7f60a1b3552cbc41007afc51ae42fd7
RobuxgamingGamer <8wfq7hpy4z@privaterelay.appleid.com>
2026-01-09 13:29:48 +0200

    Update fallback.js

--- fallback.js
+++ fallback.js
@@ -1,3 +1,5 @@
-function fallbackResponse() {
-  return "Sorry, I cannot understand this yet, I am still learning and trying to improve.";
-}
+const Fallback = {
+  reply() {
+    return "Sorry, I cannot understand this yet. I am still learning and trying to improve.";
+  }
+};
